8b6423cb986a9a0079330d8181f0f4a74814a3bd
Cao Chi Hai <haicao2805@gmail.com>
2022-03-21 16:09:08 +0700

    implements some basic features

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # google-sheet-apis
+
+1/ Enable Google Sheets API
+2/ Config OAuth consent screen
+3/ Create service Accounts
+4/ Add keys
--- index.ts
+++ index.ts
@@ -1,1 +1,37 @@
-console.log("Init");
+import { JWT } from 'google-auth-library';
+import { google, sheets_v4 } from 'googleapis';
+import { GoogleSheetApisProps, ParamsResourceSpreadsheetsValuesBatchget } from './interfaces';
+
+export class GoogleSheetApis {
+      private auth: JWT;
+      private sheets: sheets_v4.Sheets;
+
+      constructor(props: GoogleSheetApisProps) {
+            this.auth = new google.auth.JWT({
+                  keyId: props.keyId,
+                  key: props.key,
+                  email: props.email,
+                  scopes: props.scopes,
+            });
+            this.sheets = google.sheets('v4');
+      }
+
+      /**
+       *
+       * @description
+       * @param params
+       * @returns
+       */
+      public async getData(params: ParamsResourceSpreadsheetsValuesBatchget) {
+            try {
+                  const result = await this.sheets.spreadsheets.values.batchGet({
+                        spreadsheetId: params.spreadsheetId,
+                        ranges: [`${params.tabName}!${params.startColumn}:${params.endColumn}`],
+                        auth: this.auth,
+                  });
+                  return result.data.valueRanges[0].values as string[][];
+            } catch (e) {
+                  return null;
+            }
+      }
+}
